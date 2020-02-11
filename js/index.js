7495f6f9f5a2f877ad10e8a8a57bc2c3bb435af9
JGPico <minq_2@hotmail.com>
2020-02-11 15:39:49 -0700

    event 9 complete

--- js/index.js
+++ js/index.js
@@ -16,14 +16,12 @@ introBox.addEventListener('click', (event) => {
 });
 
 // prevent default for home click
-
 const stopLink = document.querySelector('.nav-link');
 stopLink.addEventListener('click', (event) => {
     event.preventDefault();
 });
 
 // event 2
-
 document.querySelectorAll('h2').forEach( el => {
     el.addEventListener('mouseenter', () => {
         el.style.color = 'blue';
@@ -32,6 +30,51 @@ document.querySelectorAll('h2').forEach( el => {
 
 // event 3
 const imgChange = document.querySelector('.img-content img');
-imgChange.addEventListener('mouseleave', () => {
+imgChange.addEventListener('mouseenter', () => {
     imgChange.src = 'img/bus2.jpg';
 });
+imgChange.addEventListener('mouseleave', () => {
+    imgChange.src = 'img/adventure.jpg';
+})
+
+// event 4
+const pChange = document.querySelectorAll('p');
+
+pChange.forEach(el => {
+    document.addEventListener('keydown', () => {
+        el.style.color = 'dodgerblue';
+    });
+
+// event 5
+    document.addEventListener('keyup', () => {
+        el.style.color = 'black';
+    });
+})
+
+// event 6 - ask about nav bar overlap
+const imgResize = document.querySelector('.inverse-content img');
+window.addEventListener('resize', () => {
+    imgResize.style.transform = 'scale(1.2)';
+    imgResize.style.transition = 'transform 0.3s';
+})
+
+// event 7
+window.addEventListener('scroll', () => {
+    imgResize.style.transform = 'scale(1)';
+    imgResize.style.transition = 'transform 0.3s';
+})
+
+// event 8
+const homeBackGround = document.querySelector('body');
+const imgRoller = document.querySelector('.imgRoll');
+imgRoller.addEventListener('mousemove', (event) => {
+    homeBackGround.style.backgroundColor = "rgb("+event.offsetX+","+event.offsetY+",60)";
+    document.querySelector('.home').style.backgroundColor = 'white';
+})
+
+// event 9
+pChange.forEach(el => {
+    document.addEventListener('dblclick', () => {
+        el.style.fontWeight = 'bold';
+    })
+})
