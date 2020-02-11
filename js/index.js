26281f9b5a65b344b77ae1d1797f6e471f6e0480
JGPico <minq_2@hotmail.com>
2020-02-11 13:38:36 -0700

    propagation stop and prevent default added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+// event 1 with propagation stop.
+const introH2 = document.querySelector('.intro h2');
+
+introH2.addEventListener('click', (event) => {
+    introH2.style.color = 'orange';
+    event.stopPropagation();
+});
+
+const introBox = document.querySelector('.intro');
+
+introBox.addEventListener('click', () => {
+    introBox.style.backgroundColor = 'grey';
+});
+
+// prevent default for home click
+
+// const stopLink = document.querySelector('.nav-link');
+// stopLink.addEventListener('click', (event) => {
+//     event.preventDefault();
+// });
